514145fa24265f9c445fcb659237645110dbdaad
Francis Perezimo <francisjeffersonbright@gmail.com>
2023-09-25 12:05:32 +0100

    Task-Manager

---
+++ script.js
@@ -0,0 +1,17 @@
+const taskform = document.getElementById('task-form');
+const taskinput = document.getElementById('task-input');
+const tasklist = document.getElementById('task-list');
+
+
+function HandleSubmit() {
+    const tasktext = taskinput.value.trim();
+    
+
+if (tasktext !== '') {
+    // addTask(tasktext)
+    taskinput.value = '',
+    taskinput.focus()
+}else{
+    alert('please, fill in the input field')
+};
+}
